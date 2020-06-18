ed5a2f14a445b0de1b0b90564cf02ff5d238e5bc
corya4 <corya4@naver.com>
2020-06-19 00:27:57 +0900

    BOJ_1065

--- JAVA/BOJ_1065.java
+++ JAVA/BOJ_1065.java
@@ -9,15 +9,55 @@ public class BOJ_1065 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int num = Integer.parseInt(br.readLine());
-		int count = 0;
 		
-		if(num < 100) {
-			count = num;
-		}else {
-			String temp = String.valueOf(num);
-		}
-
-		System.out.println(count);
+		System.out.println(solv(num));
+		
 	}
 
+	static int solv(int n) {
+
+		int count = 0;
+		
+		for(int i = 1; i <= n; i++) {
+			if(Sequence(i)) count++;
+			
+		}
+		
+		return count;
+	}
+	
+	static boolean Sequence(int c) {
+		
+		if(c < 100) return true;
+		
+		int digit = String.valueOf(c).length();
+		int div = 0;
+		int d_temp = 1001;
+		int calc = 1001;
+		int c_temp = c;
+		
+		for(int i = digit-1; i >= 0; i--) {
+			int ctn = (int)Math.pow(10, i);
+			div= c_temp / ctn;
+			c_temp %= ctn;
+			
+			if(d_temp == 1001) {
+				d_temp = div;
+				continue;
+			}
+			
+			if(calc == 1001) {
+				calc = div - d_temp;
+			}else if(calc != (div - d_temp)) {
+				return false;
+			}
+			d_temp = div;
+		}
+		
+		return true;
+	}
 }
+
+
+
+
