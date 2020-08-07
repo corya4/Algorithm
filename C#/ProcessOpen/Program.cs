4f472c861a5871fbd79016bb2a279aecf511bdc7
corya4 <corya4@naver.com>
2020-08-07 20:08:08 +0900

    CMD

---
+++ C#/ProcessOpen/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ProcessOpen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FTPProcess FP = new FTPProcess();
+
+            String commend = "";
+
+            while (!commend.Equals("exit"))
+            {
+
+                commend = Console.ReadLine();
+
+                FP.OrderCommend(commend);
+                Console.WriteLine(FP.ResponString());
+
+            }
+
+            FP.Close();
+        }
+    }
+}
